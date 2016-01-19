202551f357532e3a4f31bc03831d9445c5fdd9d5
vhchung <chung.k54thb@gmail.com>
2016-01-19 10:14:18 +0700

    - Edit SALT string
    - Add login link to header

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ if (PHP_SAPI == 'cli-server') {
 }
 
 require __DIR__ . '/../vendor/autoload.php';
-define('SALT', '123234345456567678789890098987867645432321');
+define('SALT', 'Asa*Ansa0-q%#@ASF!AS');
 session_start();
 use App\Libs\NotFoundHandler;
 // Instantiate the app
